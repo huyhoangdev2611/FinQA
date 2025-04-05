28b8f34e283581f6acec22efb18947b10c7d2c54
huyhoangdev2611 <152195071+huyhoangdev2611@users.noreply.github.com>
2025-04-05 15:44:41 +0700

    Update config.py

--- code/retriever/config.py
+++ code/retriever/config.py
@@ -29,7 +29,7 @@ class parameters():
     # private: for testing private test data
     device = "cuda"
     mode = "train"
-    resume_model_path = "/kaggle/input/model-retriever-54/model_54.pt"
+    resume_model_path = "/kaggle/input/model-retriever-110/model_110.pt"
 
     ### to load the trained model in test time
     saved_model_path = output_path + \
