f0ead0fba0cea76d3ad5e85cca1147d92182d9e6
huyhoangdev2611 <152195071+huyhoangdev2611@users.noreply.github.com>
2025-04-10 09:17:20 +0700

    Update config.py

--- code/retriever/config.py
+++ code/retriever/config.py
@@ -32,7 +32,8 @@ class parameters():
     resume_model_path = "/kaggle/input/model-retriever-110/model_110.pt"
 
     ### to load the trained model in test time
-    saved_model_path = "/kaggle/input/model-retriever-110/model_110.pt"
+    saved_model_path = output_path + \
+         "/bert-base-6k_20210427232814/saved_model/loads/3/model.pt"
     build_summary = False
 
     option = "rand"
